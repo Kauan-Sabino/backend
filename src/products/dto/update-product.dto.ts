9fae0464e848f7cc561568daf04d678218550c68
Kauan-Sabino <kauan.oliveirakso@gmail.com>
2025-09-05 15:48:44 -0300

    backv2

---
+++ src/products/dto/update-product.dto.ts
@@ -0,0 +1,13 @@
+import {  IsNumber, IsOptional, IsString, Min } from "class-validator";
+
+export class UpdateProductDto{
+
+    @IsString()
+    @IsOptional()
+    name: string;
+
+    @IsNumber()
+    @Min(0)
+    @IsOptional()
+    price:number;
+}
